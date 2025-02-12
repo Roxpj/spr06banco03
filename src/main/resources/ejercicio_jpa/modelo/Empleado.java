b1a0d706b53722b8adedd81095a4cee4e58aaca0
Roxpj <roxanapj.92@gmail.com>
2025-02-12 12:24:43 +0100

    update resources

---
+++ src/main/resources/ejercicio_jpa/modelo/Empleado.java
@@ -0,0 +1,32 @@
+package es.cursosprhib.jpa03ejerciciojpa.modelo;
+
+public class Empleado extends Persona {
+	private static final long serialVersionUID = 1L;
+
+	private Integer nroEmpleado;
+	private Categoria categoria;
+
+	public Empleado() {
+	}
+
+	public Integer getNroEmpleado() {
+		return nroEmpleado;
+	}
+
+	public void setNroEmpleado(Integer nroEmpleado) {
+		this.nroEmpleado = nroEmpleado;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	@Override
+	public String toString() {
+		return "Empleado [" + nroEmpleado + ", " + categoria + ", " + getIdPersona() + ", " + getApellidos() + "]";
+	}
+}
